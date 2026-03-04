5506803444db04822c85916b436c041285dc30f0
Gabriel Ferres <bielferres@gmail.com>
2026-03-04 06:20:54 -0300

    hide navigationbar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,7 @@
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import { Stack } from 'expo-router'
+import * as NavigationBar from 'expo-navigation-bar'
+
 import {
   useFonts,
   Inter_700Bold,
@@ -16,14 +18,25 @@ import { colors } from '@/theme/colors'
 import { Loading } from '@/components/Loading'
 
 export default function Layout() {
+  useEffect(() => {
+    async function hideNavigationBar() {
+      await NavigationBar.setVisibilityAsync("hidden");
+      await NavigationBar.setBehaviorAsync("overlay-swipe");
+    }
+    
+    hideNavigationBar();
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_700Bold,
     Inter_500Medium,
     Inter_400Regular
   })
+
   if (!fontsLoaded) {
     return <Loading />
   }
+
   return (
     <Suspense fallback={<Loading />}>
       <SQLiteProvider databaseName="targets.db" onInit={migrate} useSuspense>
